ea7bbef949e9adec1145c8f5069f16e79249434f
Jacob Lane <jvlane@uark.edu>
2024-04-20 18:30:31 -0500

    got images to work

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,21 +1,23 @@
 package com.example.application.views;
 
+import com.example.application.Card;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Route("")
 public class MainLayout extends VerticalLayout {
-    //private static final long serialVersionUID = 1L;
-    // ??? ^ FOR IMAGE BUT DOESN'T WORK
+
+    private ArrayList<Card> deck;
+
     public MainLayout() {
 
         // page header
@@ -26,6 +28,18 @@ public class MainLayout extends VerticalLayout {
         // Start Game Button (loads everything in, creates deck, etc)
 
         // TO DO ~~~~~~~~~~~~~~~~~~~~~
+
+        // add Start Game button
+        Button startGameButton = new Button("Start Game");
+        add(startGameButton);
+
+        // clickListener for startGameButton
+        startGameButton.addClickListener(event -> {
+
+            // generates decks
+            deckGenerator();
+
+        });
 
         // input for bet amount (unit size)
         // sizes: $25, $50, $75, $100, $150, $200, $250, $300
@@ -43,14 +57,35 @@ public class MainLayout extends VerticalLayout {
         hL1.add(betAmtDropdown);
         add(hL1);
 
-        // Game Section, Deal Cards and whatnot
+        // Game Section, Dealt Cards and whatnot
 
         // TO DO ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        // HorizontalLayout for cards dealt
+        // HOW TO MAKE 2 HORIZONTAL LAYOUTS SIDE BY SIDE FOR DEALER THEN OUT HANDS
+        HorizontalLayout playerHandDealt = new HorizontalLayout();
+        playerHandDealt.setWidthFull();
+        HorizontalLayout dealerHandDealt = new HorizontalLayout();
+        dealerHandDealt.setWidthFull();
+        HorizontalLayout bothHandsDealt = new HorizontalLayout(playerHandDealt, dealerHandDealt);
+        bothHandsDealt.setWidthFull();
+        add(bothHandsDealt);
+
+        // add card images
+        /*
         Image image = new Image("images/queen_of_spades.png", "Card");
-        image.setSizeFull();
-        add(image);
-        // Image image = new Image("images/myimage.png", "My Alt Image");
-        // add(image);
+        image.setWidth("100px");
+        playerHandDealt.add(image);
+        Image image3 = new Image("images/king_of_hearts.png", "Card");
+        image3.setWidth("100px");
+        image3.getStyle().set("margin-left", "-90px");
+        playerHandDealt.add(image3);
+
+        Image image2 = new Image("images/2_of_spades.png", "Card");
+        image2.setWidth("100px");
+        dealerHandDealt.add(image2);
+
+         */
 
         // Recommendation, Win Probability, Bet Size Projection
 
@@ -85,9 +120,48 @@ public class MainLayout extends VerticalLayout {
         // MAKE TO WHERE hL2 or hL3 ONLY SHOWS US
         add(hL3);
 
+        // Deal initial hands
+        confirmBetButton.addClickListener(event -> {
+
+            String value = deck.get(0).getValue();
+            String suit = deck.get(0).getSuit();
+
+            // FIGURE OUT HOW TO ADD CARDS
+
+        });
 
 
         // create reset button???
 
     }
+
+    private void deckGenerator() {
+
+        deck.clear();
+
+        ArrayList<String> suits = new ArrayList<>();
+        suits.add("clubs"); suits.add("diamonds");
+        suits.add("hearts"); suits.add("spades");
+
+        ArrayList<String> values = new ArrayList<>();
+        for (int i = 2; i <= 10; i++) {
+            values.add(String.valueOf(i));
+        }
+        values.add("ace"); values.add("jack");
+        values.add("queen"); values.add("king");
+
+        // number of decks = 20 --> large so player can play nonstop
+        for (int i = 0; i < 20; i++) {
+            for (String suit : suits) {
+                for (String value : values) {
+                    Card card = new Card(suit, value);
+                    deck.add(card);
+                }
+            }
+        }
+
+        // shuffle deck
+        Collections.shuffle(deck);
+    }
+
 }
